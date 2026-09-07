de291955fb0be5aaa43b5f10d239f5ded191965d
monaapandey <monapandey375@gmail.com>
2026-09-07 21:26:45 +0530

    Refactor loop for calculating total price

--- PriceCalculator.cpp
+++ PriceCalculator.cpp
@@ -1,11 +1,17 @@
+#include <vector>
+using namespace std;
+
+class ShowSeat;
+class Seat;
+
 class PriceCalculator {
 public:
     double calculateTotal(const vector<ShowSeat*>& seats) const {
         double total = 0;
-
-        for (ShowSeat* showSeat : seats)
-            total += showSeat->getSeat()->getPrice();
-
+        for (ShowSeat* seat : seats) {
+            total += seat->getSeat()->getPrice();
+        }
         return total;
     }
+
 };
