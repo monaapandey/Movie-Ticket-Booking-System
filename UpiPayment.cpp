09776dea927d995681c2396c007a5fccda0dac85
monaapandey <monapandey375@gmail.com>
2026-09-07 21:29:19 +0530

    Update UpiPayment.cpp

--- UpiPayment.cpp
+++ UpiPayment.cpp
@@ -1,12 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Payment;
+
 class UpiPayment : public Payment {
 public:
     bool pay(double amount) override {
+        cout << "\n[UPI] Rs." << fixed << setprecision(0) << amount << "\n";
+        cout << "Enter 1 for successful payment, 0 for failed payment: ";
         int result;
-
-        cout << "\nUPI Payment Amount: Rs." << amount << endl;
-        cout << "Enter 1 for SUCCESS or 0 to simulate FAILURE: ";
         cin >> result;
-
-        return result == 1;
+        if (result == 1) {
+            cout << "[UPI] Rs." << amount << " paid successfully\n";
+            return true;
+        }
+        cout << "[UPI] Payment failed\n";
+        return false;
     }
 };
